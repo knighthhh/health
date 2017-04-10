27a0264275ab759ba09f2a65851bbbe260749d2a
Zh3Wang <377762305@qq.com>
2017-04-10 19:02:33 +0800

    do something

--- management/Application/Connector/Controller/DocController.class.php
+++ management/Application/Connector/Controller/DocController.class.php
@@ -4,5 +4,62 @@ namespace Connector\Controller;
 use Think\Controller;
 
 class DocController extends Controller{
-	
+	//获得热门医生列表
+	public function getHotDoc(){
+    	$model = D('Admin/doctor_info');
+        $ic = C('IMAGE_CONFIG');
+    	$data = $model -> getHotDoc();
+        for ($i=0; $i <count($data) ; $i++) { 
+            $data[$i]['doc_img'] = $ic['viewPath'].$data[$i]['doc_img'];
+        }
+    	//dump($data);die;
+    	echo json_encode($data);
+    }
+
+    //获得医生详情
+    public function getDocDetail(){
+        $doc_id = I('get.doc_id');
+        $model = D('Admin/doctor_info');
+        $ic = C('IMAGE_CONFIG');
+        $info = $model -> getDocDetail($doc_id);
+        $info['doc_img'] = $ic['viewPath'].$info['doc_img'];
+        //dump($info);die;
+        echo json_encode($info);
+    }
+
+    //按医生姓名列表查找功能
+    public function getDocName(){
+        $model = D('Admin/doctor_info');
+        $data = $model
+        ->field("doc_id,doc_name")
+        ->select();
+        $i = 0;
+        foreach ($data as $k => $v) {
+            $firstCharter = mb_substr($v['doc_name'],0,1);
+            $firstCharter = getFirstCharter($firstCharter);
+            $data[$i]['firstCharter'] = $firstCharter;
+            $i++;
+        }
+        $arr = array();
+        foreach ($data as $k=> $v) {
+            $arr[] = $v['firstCharter'];
+        }
+        array_multisort($arr,SORT_ASC,$data);
+        $i = 0;
+        foreach ($data as $k => $v) {
+             if(!in_array($v['firstCharter'],$info[$i-1])){
+                 $info[$i][] = $v['firstCharter'];
+                 $i++;
+              }
+        }
+        foreach ($info as $k => $v) {
+            foreach ($data as $k1 => $v1) {
+                if($v1['firstCharter']==$info[$k][0])
+                    $info[$k]['doc'][] = $v1;
+                //echo $v1['firstCharter'];
+            }
+        }
+        //dump($info);die;
+        echo json_encode($info);
+    }
 }
--- management/Application/Connector/Controller/HosController.class.php
+++ management/Application/Connector/Controller/HosController.class.php
@@ -4,7 +4,57 @@ namespace Connector\Controller;
 use Think\Controller;
 
 class HosController extends Controller{
-	
+	//获得热门医院列表
+	public function getHotHos(){
+    	$model = D('Hospital_info');
+        $ic = C('IMAGE_CONFIG');
+        //dump($ic['viewPath']);
+    	$data = $model->select();
+        for ($i=0; $i <count($data) ; $i++) { 
+            $data[$i]['hos_navigate_img'] = $ic['viewPath'].$data[$i]['hos_navigate_img'];
+        }
+        //dump($data);die;
+    	echo json_encode($data);
+    }
+
+    //获得医院详情
+     public function getHosDetail(){
+        $hos_id = I('get.hos_id');
+        $model = D('Admin/hospital_info');
+        $info = $model -> getHosDetail($hos_id);
+        //dump($info);die;
+        echo json_encode($info);
+    }
+
+    //获得医院详情轮播图片
+    public function getHosImg(){
+        $hos_id = I('get.hos_id');
+        $model = D('hos_img');
+        $info = $model->where(array(
+            'hos_id' => array('eq',$hos_id),
+            ))->select();
+        $ic = C('IMAGE_CONFIG');
+        for ($i=0; $i <count($info) ; $i++) { 
+            $info[$i]['hos_img'] = $ic['viewPath'].$info[$i]['hos_img'];
+            $info[$i]['hos_sm_img'] = $ic['viewPath'].$info[$i]['hos_sm_img'];
+            $info[$i]['hos_mid_img'] = $ic['viewPath'].$info[$i]['hos_mid_img'];
+        }
+        //dump($info);die;
+        echo json_encode($info);
+    }
+
+    //获得所有医院列表
+    public function getHos(){
+        $model = D('Hospital_info');
+        $ic = C('IMAGE_CONFIG');
+        //dump($ic['viewPath']);
+        $data = $model->select();
+        for ($i=0; $i <count($data) ; $i++) { 
+            $data[$i]['hos_navigate_img'] = $ic['viewPath'].$data[$i]['hos_navigate_img'];
+        }
+        //dump($data);die;
+        echo json_encode($data);
+    }
 
 	
 }
