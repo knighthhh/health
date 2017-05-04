741c883325173382dee9043654d45564db374e4b
Zh3Wang <377762305@qq.com>
2017-05-04 23:33:58 +0800

    整合

--- management/Application/Runtime/Cache/Admin/f5bbdcfbdd89e3929c107b2480f6f946.php
+++ management/Application/Runtime/Cache/Admin/f5bbdcfbdd89e3929c107b2480f6f946.php
@@ -22,7 +22,7 @@
 </style>
 </head>
 <body>
-<form method="post" action="/health/management/Application/index.php/Admin/User/listUser" id="listform">
+<form method="post" action="/health/management/Application/index.php/Admin/User/listUser/p/3.html" id="listform">
   <div class="panel admin-panel">
     <div class="panel-head"><strong class="icon-reorder"> 内容列表</strong></div>
     <div class="padding border-bottom">
